9c43c2ddef00ae307159f75f43ed65fabb473b0a
Jean Carlo Deretti <jeanderetti@weg.net>
2022-02-03 21:10:01 -0300

    Change db connection

--- src/service/model/User.js
+++ src/service/model/User.js
@@ -1,17 +1,14 @@
-const typeorm = require('typeorm')
-const dbConfig = require('../../configuration/db')
-
 const connection = require('../../connection/db')
 
 module.exports = (function () {
 
   function createAndUpdateUser(request) {
     return new Promise((resolve, reject) => {
-      typeorm.createConnection(dbConfig).then(conn => {
+      connection.conn().then(conn => {
         const userRepository = conn.getRepository('User')
 
         userRepository.findByIds([request.body.id])
-          .then(users => {
+          .then((users) => {
             let params = { NM_USER: request.body.name }
 
             if (users.length) params['CD_USER'] = request.body.id
@@ -19,55 +16,42 @@ module.exports = (function () {
             userRepository
               .save(params)
               .then(() => { return resolve((users.length) ? 'User updated' : 'User created') })
-              .catch(err => { return reject(err) })
-              .finally(() => conn.close())
-          }).catch(err => {
-            conn.close()
-
-            return reject(err)
-          })
-      }).catch(() => { return reject('There was an error connecting to the database') })
+              .catch((err) => { return reject(err) })
+          }).catch((err) => { return reject(err) })
+      }).catch((err) => { return reject(err) })
     })
   }
 
   function readUser(request) {
     return new Promise((resolve, reject) => {
-      // typeorm.createConnection(dbConfig).then(conn => {
-      // const userRepository = conn.getRepository('User')
-      (async () => {
-        const conn = await connection.conn()
+      connection.conn().then((conn) => {
         const userRepository = conn.getRepository('User')
 
         if (request.query.id) {
           userRepository
             .findByIds([request.query.id])
-            .then(users => { return resolve(users) })
-            .catch(err => { return reject(err) })
-            .finally(() => conn.close())
+            .then((users) => { return resolve(users) })
+            .catch((err) => { return reject(err) })
         } else {
           userRepository
             .find()
-            .then(users => { return resolve(users) })
-            .catch(err => { return reject(err) })
-          // .finally(() => conn.close())
-          // .finally(() => connection.conn().close())
+            .then((users) => { return resolve(users) })
+            .catch((err) => { return reject(err) })
         }
-      })()
-      // }).catch(() => { return reject('There was an error connecting to the database') })
+      }).catch((err) => { return reject(err) })
     })
   }
 
   function deleteUser(request) {
     return new Promise((resolve, reject) => {
-      typeorm.createConnection(dbConfig).then(conn => {
+      connection.conn().then((conn) => {
         const userRepository = conn.getRepository('User')
 
         userRepository
           .delete({ CD_USER: request.body.id })
-          .then(res => { return resolve((res.affected) ? 'user deleted' : 'invalid id') })
-          .catch(err => { return reject(err) })
-          .finally(() => conn.close())
-      }).catch(() => { return reject('There was an error connecting to the database') })
+          .then((res) => { return resolve((res.affected) ? 'user deleted' : 'invalid id') })
+          .catch((err) => { return reject(err) })
+      }).catch((err) => { return reject(err) })
     })
   }
 
